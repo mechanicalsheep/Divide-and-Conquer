c0b66bbfa8e57af39296c5b6d672a8b8298f87b2
mechanicalsheep <learn@sheepy.ca>
2016-12-21 11:23:28 +0300

    Recent uncomplete Merger
    broken Merger, will erase all merger and use testing class one to transform.

--- Merger.cs
+++ Merger.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,56 +9,78 @@ using System.IO;
 
 namespace Divide_and_Conquer
 {
-    class Merger
+    class Merger : MainForm
     {
         string dest;
-        public Merger()
+        string[] filePaths;
+        public Merger(MainForm f, string sourceFolder, string[] englishPaths)
         {
-            dest = @"D:\Homeworks\testing struct\to merge";
+            dest = sourceFolder;
+            MainForm form = f;
+            filePaths = englishPaths;
+
+
         }
 
         public void fileGetter()
         {
             DirectoryInfo accessfile = new DirectoryInfo(dest);
         }
-           public void Merge()
-{
+
+
+        public void Merge()
+        {
             /// <summary>
             /// Imports all pages from a list of documents.
             /// </summary>
             ///
             // Get some file names
-            /* 
-             * COMMENTING CODE TO TEST SPLITTER
-            string[] files = GetFiles();
 
-                // Open the output document
-                PdfDocument outputDocument = new PdfDocument();
+            //NOT NEEDED, TEST ON filePaths[0] for now // string[] files = GetFiles();
 
-                // Iterate files
-                foreach (string file in files)
+            // Open the output document
+            PdfDocument outputDocument = new PdfDocument();
+
+            // Iterate files
+
+            DirectoryInfo folder = new DirectoryInfo(dest);
+            foreach (FileInfo file in folder.GetFiles())
+            {
+                // Open the document to import pages from it.
+                PdfDocument inputDocument = PdfReader.Open(file.FullName, PdfDocumentOpenMode.Import);
+
+                // Iterate pages
+                int count = inputDocument.PageCount;
+                for (int idx = 0; idx < count; idx++)
                 {
-                    // Open the document to import pages from it.
-                    PdfDocument inputDocument = PdfReader.Open(file, PdfDocumentOpenMode.Import);
+                    // Get the page from the external document...
+                    PdfPage page = inputDocument.Pages[idx];
+                    // ...and add it to the output document.
+                    outputDocument.AddPage(page);
+                   
+                }
+                //close current inputdocument
+                inputDocument.Close();
+                PdfDocument englishDocument = PdfReader.Open(filePaths[0], PdfDocumentOpenMode.Import);
+                count = englishDocument.PageCount;
+                for (int idx = 0; idx < count; idx++)
+                {
+                    // Get the page from the external document...
+                    PdfPage page = englishDocument.Pages[idx];
+                    // ...and add it to the output document.
+                    outputDocument.AddPage(page);
 
-                    // Iterate pages
-                    int count = inputDocument.PageCount;
-                    for (int idx = 0; idx < count; idx++)
-                    {
-                        // Get the page from the external document...
-                        PdfPage page = inputDocument.Pages[idx];
-                        // ...and add it to the output document.
-                        outputDocument.AddPage(page);
-                    }
                 }
 
+
                 // Save the document...
-                const string filename = "ConcatenatedDocument1_tempfile.pdf";
+                const string filename = "D:\\ConcatenatedDocument1_tempfile.pdf";
                 outputDocument.Save(filename);
                 // ...and start a viewer.
-                Process.Start(filename);*/
+               // Process.Start(filename);
             }
 
-        
+
+        }
     }
 }
